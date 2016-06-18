281c125faac745af103dc4dbc52b4eae09374119
sanjeev singh <snjvsingh123@gmail.com>
2016-06-18 12:59:52 -0400

    added features

---
+++ spec/features/authentication_pages_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe "Authentication" do
+  subject { page }
+
+  describe "signin page" do
+    before { visit signin_path }
+
+    it { should have_selector('h1', text: "Sign in") }
+  end
+end
